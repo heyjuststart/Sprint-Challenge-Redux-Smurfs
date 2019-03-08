77ff3a01f3536ef319bd028325c85f3f5b24bc89
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-03-08 11:28:17 -0500

    add action constants, initial reducer

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,4 +1,4 @@
-/* 
+/*
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
@@ -13,3 +13,13 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const ADD_SMURF            = 'ADD_SMURF';
+export const FETCH_SMURFS         = 'FETCH_SMURFS';
+export const UPDATE_SMURF         = 'UPDATE_SMURF';
+export const DELETE_SMURF         = 'DELETE_SMURF';
+export const ADD_SMURF_SUCCESS    = 'ADD_SMURF_SUCCESS';
+export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
+export const UPDATE_SMURF_SUCCESS = 'UPDATE_SMURF_SUCCESS';
+export const DELETE_SMURF_SUCCESS = 'DELETE_SMURF_SUCCESS';
+export const REQUEST_FAILURE      = 'REQUEST_FAILURE';
+
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,12 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import {
+  ADD_SMURF,
+  FETCH_SMURFS,
+  UPDATE_SMURF,
+  DELETE_SMURF
+} from '../actions';
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
@@ -14,10 +20,26 @@
  }
 */
 
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+};
+
 /*
   You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
+  Feel free to export it as a default and import as rootReducer.
   This will guard your namespacing issues.
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
